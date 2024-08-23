37165c49bdc38c9fd8b844a74e8671c6b730a9fd
PitaNuno <jose.netto4@fatec.sp.gov.br>
2024-08-22 22:06:22 -0300

    mensga

--- src/main/java/com/programacao/web/fatec/api_fatec/controllers/ExercicioController.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controllers/ExercicioController.java
@@ -1,5 +1,6 @@
 package com.programacao.web.fatec.api_fatec.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,6 +8,27 @@ public class ExercicioController {
 
     @GetMapping
     public String HelloWord(){
-        return "nome12";
+        return "Home";
     }
+
+    @GetMapping ("/endpoint1")
+    public String endPointMethod1(){
+        return "END POINT 1";
+    }
+
+    @GetMapping ("/endpoint2/{name}")
+    public String invertName(@PathVariable String name){
+        return new StringBuilder(name).reverse().toString();
+    }
+
+    @GetMapping("/endpoint3/{number}")
+    String verificarParOuImpar(@PathVariable Integer number) {
+        if (number % 2 == 0) {
+            return "Par";
+        } else {
+            return "Ímpar";
+        }
+    }
+
+
 }
